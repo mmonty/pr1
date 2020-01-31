e42f46d740265ebce7f24e1ec027f7e8ff0c55df
Martin Alessandro Montenegro <mmontene@ecegrid-thin1.ecn.purdue.edu>
2020-01-31 00:30:15 -0500

    hello world2

--- controller.c
+++ controller.c
@@ -1,38 +1,77 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 
+static int sock_bind(int domain, int type, const char *hostname, const char *servname);
+static void print_sockaddr(struct sockaddr *addr);
+
 int main(int argc, char *argv[])
+{
+	int 		s;
+	char 		hello[6];
+	struct sockaddr	peer;
+	socklen_t 	peer_len = sizeof(struct sockaddr_in);
+
+	if (argc != 3) {
+		printf("usage: ./controller hostname portnumber");
+		exit(EXIT_FAILURE);
+	}
+
+	s = sock_bind(AF_INET, SOCK_DGRAM, argv[1], argv[2]);
+
+	//test
+	recvfrom(s, hello, 6, 0, &peer, &peer_len);
+		
+	printf("received from:\n");
+	print_sockaddr(&peer);
+	printf("message: \n");
+	fwrite(hello, 1, 6, stdout);
+
+	close(s);
+	exit(EXIT_SUCCESS);
+}
+
+static void
+print_sockaddr(struct sockaddr *addr) {
+
+        struct sockaddr_in *ipname;
+        char ipstr[INET_ADDRSTRLEN];
+
+        ipname = (void *)addr;
+        inet_ntop(AF_INET, &ipname->sin_addr, ipstr, INET_ADDRSTRLEN);
+        printf("address:%s\n", ipstr);
+
+        return;
+}
+
+static int 
+sock_bind(int domain, int type, const char *hostname, const char *servname) 
 {
 	int s;
 	int addr_err;
 	struct addrinfo hints;
 	struct addrinfo *res;
 
-	if (argc != 3) {
-		printf("usage: ./controller hostname portnumber")
-		exit(EXIT_FAILURE);
-	}
-
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_INET;    
-	hints.ai_socktype = SOCK_DGRAM; 
+	hints.ai_family = domain;    
+	hints.ai_socktype = type; 
 	hints.ai_flags = 0;    
 	hints.ai_protocol = 0;        
 	hints.ai_canonname = NULL;
 	hints.ai_addr = NULL;
 	hints.ai_next = NULL;
 
-	addr_err = getaddrinfo(argv[1], argv[2], &hints, &res);
+	addr_err = getaddrinfo(hostname, servname, &hints, &res);
 	if (addr_err != 0) {
 		fprintf(stderr, "controller failed getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
 
-	s = socket(AF_INET, SOCK_DGRAM, 0);
+	s = socket(domain, type, 0);
 	if (s == -1) {
 		perror("Controller failed to allocate socket");
 		exit(EXIT_FAILURE);
@@ -45,8 +84,5 @@ int main(int argc, char *argv[])
 
 	freeaddrinfo(res);
 
-
-
-	close(s);
-	exit(EXIT_SUCCESS);
+	return s;
 }
--- switch.c
+++ switch.c
@@ -7,27 +7,34 @@
 #include <netdb.h>
 
 static int sock_bind(int domain, int type, const char *hostname, const char *servname);
+static void print_sockaddr(struct sockaddr* addr);
+static struct addrinfo *get_hostaddr(const char* hostname, const char *servname);
 
-int main(int argc, char *argv[])
+int 
+main(int argc, char *argv[])
 {
 	int s;
+	char hello[6] = "hello";
+	
+	struct addrinfo *controller_addr;
 
 	if (argc != 3) {
-		printf("usage: ./controller hostname portnumber");
+		printf("usage: ./switch controller_hostname controller_portnumber");
 		exit(EXIT_FAILURE);
 	}
 
-	s = sock_bind(AF_INET, SOCK_DGRAM, argv[1], argv[2]);
+	s = sock_bind(AF_INET, SOCK_DGRAM, "0.0.0.0", NULL);
 
-	
+	controller_addr = get_hostaddr(argv[1], argv[2]);
+	sendto(s, hello, 6, 0, controller_addr->ai_addr, controller_addr->ai_addrlen);
 
+	freeaddrinfo(controller_addr);
 	close(s);
 	exit(EXIT_SUCCESS);
 }
 
-static int 
-sock_bind(int domain, int type, const char *hostname, const char *servname) 
-{
+static struct addrinfo * 
+get_hostaddr(const char* hostname, const char *servname) {
 	int s;
 	int addr_err;
 	struct addrinfo hints;
@@ -44,18 +51,56 @@ sock_bind(int domain, int type, const char *hostname, const char *servname)
 
 	addr_err = getaddrinfo(hostname, servname, &hints, &res);
 	if (addr_err != 0) {
-		fprintf(stderr, "controller failed getaddrinfo: %s\n", gai_strerror(s));
+		fprintf(stderr, "Switch failed controller getaddrinfo: %s\n", gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
+	return res;
+}
+
+static void
+print_sockaddr(struct sockaddr *addr) {
+
+	struct sockaddr_in *ipname;
+	char ipstr[INET_ADDRSTRLEN];
+
+	ipname = (void *)addr;
+	inet_ntop(AF_INET, &ipname->sin_addr, ipstr, INET_ADDRSTRLEN);
+	printf("address:%s\n", ipstr);
+
+	return;
+}
+
+static int 
+sock_bind(int domain, int type, const char *hostname, const char *servname) 
+{
+	int s;
+	int addr_err;
+	struct addrinfo hints;
+	struct addrinfo *res;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = domain;    
+	hints.ai_socktype = type; 
+	hints.ai_flags = 0;    
+	hints.ai_protocol = 0;        
+	hints.ai_canonname = NULL;
+	hints.ai_addr = NULL;
+	hints.ai_next = NULL;
+
+	addr_err = getaddrinfo(hostname, servname, &hints, &res);
+	if (addr_err != 0) {
+		fprintf(stderr, "Switch failed getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
 
 	s = socket(domain, type, 0);
 	if (s == -1) {
-		perror("Controller failed to allocate socket");
+		perror("Switch failed to allocate socket");
 		exit(EXIT_FAILURE);
 	}
 
 	if (bind(s, res->ai_addr, res->ai_addrlen) == -1) {
-		perror("Controller failed to bind socket");
+		perror("Switch failed to bind socket");
 		exit(EXIT_FAILURE);
 	}
 
